b9abe651d9733b97cff538739cd97bd1337f89bf
acelproject <aclprcjt@gmail.com>
2024-02-14 01:45:12 +0800

    edit bg

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
+import bg1 from "./assets/bg1.jpg";
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className="w-full h-[100vh] hero mx-auto flex justify-center items-center bg-black relative overflow-hidden">
+      <div className="absolute md:w-1/2 w-[500px]">
+        <img src={bg1} alt="" className="" />
+      </div>
       <div>
-        <div>
+        {/* <div>
           <Link to={`/`}>Home</Link>
         </div>
         <div>
           <Link to={`/tabs`}>Tabs</Link>
-        </div>
-        <div>
-          <Link to={`/valentine`}>Valentine</Link>
+        </div> */}
+        <div className="relative z-40 -top-20 bg-pink-600 rounded-md text-white px-5 py-2 text-2xl">
+          <Link to={`/valentine`}>Klik Dulu!</Link>
         </div>
       </div>
     </div>
